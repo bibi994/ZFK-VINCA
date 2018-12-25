620c55b1dea3b1066713d5e17332f50b5e12990a
djuretanovic <djuretanovic@gmail.com>
2018-12-25 15:36:31 +0100

    React file structure

---
+++ src/view/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+import Header from './components/common/header/Header'
+
+class App extends Component {
+  render() {
+    return (
+
+      <Header />
+      
+    )
+  }
+}
+
+export default App;
